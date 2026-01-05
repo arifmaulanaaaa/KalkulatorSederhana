a5f728fd60b1baf4143bbb7599111f30c5e55c74
arifmaulanaaaa <arif270505@gmail.com>
2026-01-05 21:58:41 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Operasi Aritmatika Sederhana");
+
+        int a = 5;
+        int b = 5;
+        int hasil = a + b;
+        System.out.println("hasilnya adalah " + hasil);
+    }
+}
