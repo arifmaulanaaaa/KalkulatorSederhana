1c99f7e7872dea86f5c6174af1678dd891d9e120
arifmaulanaaaa <arif270505@gmail.com>
2026-01-06 19:01:28 +0700

    Menambahkan fitur loop untuk memberikan pilihan pada pengguna untuk lanjut atau tidak.

--- src/Main.java
+++ src/Main.java
@@ -3,46 +3,56 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean lanjut = true;
 
-        System.out.println("=== Kalkulator Sederhana ===");
+        while (lanjut) {
+            System.out.println("=== Kalkulator Sederhana ===");
 
-        System.out.println("Masukkan angka pertama: ");
-        int angkaPertama = input.nextInt();
+            System.out.println("Masukkan angka pertama: ");
+            int angkaPertama = input.nextInt();
 
-        System.out.println("Masukkan angka kedua: ");
-        int angkaKedua = input.nextInt();
+            System.out.println("Masukkan angka kedua: ");
+            int angkaKedua = input.nextInt();
 
-        System.out.println("Masukkan Sistem Operasi: ");
-        System.out.println("1 untuk pertambahan");
-        System.out.println("2 untuk pengurangan");
-        System.out.println("3 untuk perkalian");
-        System.out.println("4 untuk pembagian");
-        int pilihan = input.nextInt();
+            System.out.println("Masukkan Sistem Operasi: ");
+            System.out.println("1 untuk pertambahan");
+            System.out.println("2 untuk pengurangan");
+            System.out.println("3 untuk perkalian");
+            System.out.println("4 untuk pembagian");
+            int pilihan = input.nextInt();
 
-        int hasil = 0;
+            int hasil = 0;
 
-        switch (pilihan) {
-            case 1:
-                hasil = angkaPertama + angkaKedua;
-                System.out.println("hasilnya adalah " + hasil);
-                break;
-
-            case 2:
-                hasil = angkaPertama - angkaKedua;
-                System.out.println("hasilnya adalah " + hasil);
-                break;
-
-            case 3:
-                hasil = angkaPertama * angkaKedua;
-                System.out.println("hasilnya adalah " + hasil);
-
-            case 4:
-                if (angkaKedua != 0) {
-                    hasil = angkaPertama / angkaKedua;
+            switch (pilihan) {
+                case 1:
+                    hasil = angkaPertama + angkaKedua;
                     System.out.println("hasilnya adalah " + hasil);
-                } else {
-                    System.out.println("Pembagian dengan nol tidak valid!");
-                }
+                    break;
+
+                case 2:
+                    hasil = angkaPertama - angkaKedua;
+                    System.out.println("hasilnya adalah " + hasil);
+                    break;
+
+                case 3:
+                    hasil = angkaPertama * angkaKedua;
+                    System.out.println("hasilnya adalah " + hasil);
+                    break;
+
+                case 4:
+                    if (angkaKedua != 0) {
+                        hasil = angkaPertama / angkaKedua;
+                        System.out.println("hasilnya adalah " + hasil);
+                    } else {
+                        System.out.println("Pembagian dengan nol tidak valid!");
+                    }
+                    break;
+            }
+            System.out.println("Masih mau lanjut menghitung? (yes/no)");
+            String pilihanLanjut = input.next();
+            if (pilihanLanjut.equalsIgnoreCase("no")) {
+                lanjut = false;
+            }
         }
 
         System.out.println("=== Terimakasih telah menggunakan Kalkulator ===");
